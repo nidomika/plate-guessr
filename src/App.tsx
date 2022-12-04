541e9e525316f04b69cc493fdc4fa3683881c1d8
Dominika Szymańska <dominika1872@gmail.com>
2022-12-04 17:53:42 +0100

    Init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,78 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+
+function Voivodeship() {
+  const plates = [
+    {
+      abbreviation: 'B',
+      name: 'podlaskie'
+    },
+    {
+      abbreviation: 'C',
+      name: 'kujawsko-pomorskie'
+    },
+    {
+      abbreviation: 'D',
+      name: 'dolnośląskie'
+    },
+    {
+      abbreviation: 'E',
+      name: 'łódzkie'
+    },
+    {
+      abbreviation: 'F',
+      name: 'lubuskie'
+    },
+    {
+      abbreviation: 'G',
+      name: 'pomorskie'
+    },
+    {
+      abbreviation: 'K',
+      name: 'małopolskie'
+    },
+    {
+      abbreviation: 'L',
+      name: 'lubelskie'
+    },
+    {
+      abbreviation: 'N',
+      name: 'warmińsko-mazurskie'
+    },
+    {
+      abbreviation: 'O',
+      name: 'opolskie'
+    },
+    {
+      abbreviation: 'P',
+      name: 'wielkopolskie'
+    },
+    {
+      abbreviation: 'S',
+      name: 'śląskie'
+    },
+    {
+      abbreviation: 'T',
+      name: 'świętokrzyskie'
+    },
+    {
+      abbreviation: 'W',
+      name: 'mazowieckie'
+    },
+    {
+      abbreviation: 'Z',
+      name: 'zachodniopomorskie'
+    }
+  ]
+
+  return <p>{plates[Math.floor(Math.random() * plates.length)].name}</p>
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Voivodeship></Voivodeship>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
